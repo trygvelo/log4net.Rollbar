4a2582c04775e22a1353d30bf0b74b74cbddd3b8
TrygveLorentzen <trygve@lorentzenconsult.com>
2018-03-22 17:45:08 +0100

    Changed to using official Rollbar .net library. Added sample consoleapp, just need to set AccessToken in appSettings for it to work.

---
+++ ConsoleAppFramework452/Program.cs
@@ -0,0 +1,34 @@
+﻿using Rollbar;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFramework452
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            RollbarLocator.RollbarInstance.Configure(new RollbarConfig(ConfigurationManager.AppSettings["Rollbar.AccessToken"])
+            {
+                Environment = ConfigurationManager.AppSettings["Rollbar.Environment"]
+            });
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            log.Debug("DEBUG test via log4net.Rollbar appender");
+            log.Info("INFO test via log4net.Rollbar appender");
+            log.Warn("WARN test via log4net.Rollbar appender");
+            log.Error("ERROR test via log4net.Rollbar appender");
+            log.Fatal("FATAL test via log4net.Rollbar appender");
+
+            Console.WriteLine("Press any key to end");
+            Console.Read();
+        }
+    }
+}
--- log4net.Rollbar/RollbarAppender.cs
+++ log4net.Rollbar/RollbarAppender.cs
@@ -3,111 +3,52 @@ using System.Collections.Generic;
 using System.Configuration;
 using log4net.Appender;
 using log4net.Core;
-using RollbarSharp;
-using RollbarSharp.Serialization;
-using Configuration = RollbarSharp.Configuration;
-using System.Threading.Tasks;
+using Rollbar;
 
 namespace log4net.Rollbar
 {
     public class RollbarAppender : AppenderSkeleton
     {
-        private Configuration _configuration;
-
-        public string AccessToken { get; set; }
-        public string Environment { get; set; }
-        public string Endpoint { get; set; }
-        public string Framework { get; set; }
-        public string GitSha { get; set; }
-        public string Language { get; set; }
-        public string Platform { get; set; }
-        public string ScrubParams { get; set; }
-
-        /// <summary>
-        /// Send log events to Rollbar asynchronously
-        /// </summary>
-        public bool Asynchronous { get; set; }
-
-        public RollbarAppender()
-        {
-            Asynchronous = true;
-        }
-
-        public override void ActivateOptions()
-        {
-            _configuration = new Configuration(GetConfigSetting(AccessToken, "Rollbar.AccessToken"));
-
-            _configuration.Endpoint = GetConfigSetting(Endpoint, "Rollbar.Endpoint", _configuration.Endpoint);
-            _configuration.Environment = GetConfigSetting(Environment, "Rollbar.Environment", _configuration.Environment);
-            _configuration.Framework = GetConfigSetting(Framework, "Rolllbar.Framework", _configuration.Framework);
-            _configuration.GitSha = GetConfigSetting(GitSha, "Rollbar.GitSha");
-            _configuration.Language = GetConfigSetting(Language, "Rollbar.CodeLanguage", _configuration.Language);
-            _configuration.Platform = GetConfigSetting(Platform, "Rollbar.Platform", _configuration.Platform);
-
-            var scrubParams = GetConfigSetting(ScrubParams, "Rollbar.ScrubParams");
-            _configuration.ScrubParams = scrubParams == null ?
-                Configuration.DefaultScrubParams : scrubParams.Split(',');
-        }
-
-        private static string GetConfigSetting(string param, string name, string fallback = null)
-        {
-            return param ?? ConfigurationManager.AppSettings[name] ?? fallback;
-        }
-
         /// <summary>
         /// Sends the given event to Rollbar
         /// </summary>
         /// <param name="loggingEvent">The event to report</param>
         protected override void Append(LoggingEvent loggingEvent)
         {
-            var client = new RollbarClient(_configuration);
-
-            Task task = null;
             if (loggingEvent.Level >= Level.Critical)
             {
-                task = Send(loggingEvent, client.SendCriticalMessage, client.SendCriticalException);
+                if (loggingEvent.ExceptionObject != null)
+                    RollbarLocator.RollbarInstance.Critical(loggingEvent.ExceptionObject, null);
+                else
+                    RollbarLocator.RollbarInstance.Critical(loggingEvent.RenderedMessage, null);
             }
             else if (loggingEvent.Level >= Level.Error)
             {
-                task = Send(loggingEvent, client.SendErrorMessage, client.SendErrorException);
+                if (loggingEvent.ExceptionObject != null)
+                    RollbarLocator.RollbarInstance.Error(loggingEvent.ExceptionObject, null);
+                else
+                    RollbarLocator.RollbarInstance.Error(loggingEvent.RenderedMessage, null);
             }
             else if (loggingEvent.Level >= Level.Warn)
             {
-                task = Send(loggingEvent, client.SendWarningMessage, client.SendWarningException);
+                if (loggingEvent.ExceptionObject != null)
+                    RollbarLocator.RollbarInstance.Warning(loggingEvent.ExceptionObject, null);
+                else
+                    RollbarLocator.RollbarInstance.Warning(loggingEvent.RenderedMessage, null);
             }
             else if (loggingEvent.Level >= Level.Info)
             {
-                task = client.SendInfoMessage(loggingEvent.RenderedMessage);
+                if (loggingEvent.ExceptionObject != null)
+                    RollbarLocator.RollbarInstance.Info(loggingEvent.ExceptionObject, null);
+                else
+                    RollbarLocator.RollbarInstance.Info(loggingEvent.RenderedMessage, null);
             }
             else if (loggingEvent.Level >= Level.Debug)
             {
-                task = client.SendDebugMessage(loggingEvent.RenderedMessage);
-            }
-
-            if (task != null && !Asynchronous)
-            {
-                task.Wait(TimeSpan.FromSeconds(5));
-            }
-        }
-
-        /// <summary>
-        /// Helper method for reporting a given event. Keeps the code DRY
-        /// </summary>
-        /// <param name="loggingEvent"></param>
-        /// <param name="sendMessage"></param>
-        /// <param name="sendException"></param>
-        private Task Send(
-            LoggingEvent loggingEvent,
-            Func<string, IDictionary<string, object>, Action<DataModel>, string, Task> sendMessage,
-            Func<Exception, string, Action<DataModel>, string, Task> sendException)
-        {
-            if (loggingEvent.ExceptionObject == null)
-            {
-                return sendMessage(loggingEvent.RenderedMessage, null, null, null);
-            }
-            else
-            {
-                return sendException(loggingEvent.ExceptionObject, null, null, null);
+                if (loggingEvent.ExceptionObject != null)
+                    RollbarLocator.RollbarInstance.Debug(loggingEvent.ExceptionObject, null);
+                else
+                    RollbarLocator.RollbarInstance.Debug(loggingEvent.RenderedMessage, null);
             }
         }
     }
